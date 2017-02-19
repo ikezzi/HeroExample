ccc2767b34dccd7ff602e34b4d644e625a9a7ba4
Kazuhiro Ikeda <ikezzzzzi@gmail.com>
2017-02-19 17:26:25 +0900

    create example

---
+++ HeroExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeroExample
+//
+//  Created by Kazuhiro Ikeda on 2017/02/19.
+//  Copyright © 2017年 ikezzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
